1738eae40604d8daa706934ac9bf265661098dd2
Evan Marks <evan@babyli.st>
2016-03-09 15:10:47 -0800

    Helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def find_asset(logical_path)
+    if Rails.application.config.assets.compile
+      assets.find_asset(logical_path)
+    else
+      assets_manifest.assets[logical_path]
+    end
+  end
+
 end
